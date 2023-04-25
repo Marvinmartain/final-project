ee6dec967ea05aad618c798b73008a747ef84656
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-24 22:02:45 -0400

    28commit

--- api/index.js
+++ api/index.js
@@ -68,5 +68,14 @@ app.post('/logout', (req,res) => {
 app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
   const {originalname,path} = req.file;
   const parts = originalname.split('.');
-  
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {title,summary,content} = req.body;
+    const postDoc = await Post.create({
+      
   
