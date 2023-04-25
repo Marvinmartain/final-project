5a644a9638bb6fd4764099a7e0ddc652bc63eeb9
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-24 21:59:10 -0400

    28commit

--- api/index.js
+++ api/index.js
@@ -4,7 +4,14 @@ const mongoose = require("mongoose");
 const User = require('./models/User');
 const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
 
+const salt = bcrypt.genSaltSync(10);
 
 
 
