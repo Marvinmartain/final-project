094a027464e46cd895ab97d658aeb4384e5f7c86
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-24 21:57:45 -0400

    28commit

--- api/index.js
+++ api/index.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require("mongoose");
-const User = require('./models/User');
-const Post = require('./models/Post');
-const bcrypt = require('bcryptjs');
+
 
 
 
